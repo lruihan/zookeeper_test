c3dd24fc0e621c9757d61b23827b84ce610c07e4
Rissy Lin <lrissy@vmware.com>
2017-03-27 18:08:38 +0800

    First Zookeeper Test
    QA Notes: <specific items that you would like QA to test, e.g., regression notes>
    Testing done: <e.g., sandbox URL, specific dev tests, etc.>
    Bug number:
    Jira issue:
    Reviewed by:
    Approved by:
    Review URL:
    
    Merge to: master: # YES, NO

---
+++ src/main/java/test/zookeeper/ZkConnect.java
@@ -0,0 +1,67 @@
+package test.zookeeper;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class ZkConnect {
+
+    private ZooKeeper zk;
+    private CountDownLatch connSignal = new CountDownLatch(0);
+
+    public ZooKeeper connect(String host) throws IOException, InterruptedException{
+        zk = new ZooKeeper(host, 1200000, new Watcher() {
+            public void process(WatchedEvent watchedEvent) {
+                if(watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+                    connSignal.countDown();
+                }
+            }
+        });
+        connSignal.await();
+        return zk;
+    }
+
+    public void close() throws InterruptedException {
+        zk.close();
+    }
+
+    public void createNode(String path, byte[] data) throws Exception {
+        zk.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+    }
+
+    public void updateNode(String path, byte[] data) throws Exception {
+        zk.setData(path, data, zk.exists(path, true).getVersion());
+    }
+
+    public void deleteNode(String path) throws Exception {
+        zk.delete(path, zk.exists(path, true).getVersion());
+    }
+    public static void main( String[] args ) throws Exception {
+        ZkConnect zkConnect = new ZkConnect();
+        ZooKeeper zooKeeper = zkConnect.connect("127.0.0.1");
+        String newNode = "/deepakDate";
+        zkConnect.createNode(newNode, new Date().toString().getBytes());
+        List<String> zNodes = zooKeeper.getChildren("/", true);
+        for(String zNode : zNodes) {
+            System.out.println("ChildrenNode: " + zNode);
+        }
+        byte[] data = zooKeeper.getData(newNode, true, zooKeeper.exists(newNode, true));
+        System.out.println("GetData before setting: " + new String(data));
+
+        zkConnect.updateNode(newNode, "Modified data".getBytes());
+        data = zooKeeper.getData(newNode, true, zooKeeper.exists(newNode, true));
+
+        System.out.println("GetData after setting: " + new String(data));
+
+        zkConnect.deleteNode(newNode);
+
+        zkConnect.close();
+    }
+}
